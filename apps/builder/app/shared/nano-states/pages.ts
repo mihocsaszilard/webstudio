0e46263ac7c57ec6e7a3422f03cdcab96d666d2c
CZNET\XDSMIHOC <szilard.mihocsa.ext@zeiss.com>
2024-11-25 16:30:32 +0100

    first commit

--- apps/builder/app/shared/nano-states/pages.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "nanostores";
-import type { Page, Pages } from "@webstudio-is/sdk";
-
-export const $pages = atom<undefined | Pages>(undefined);
-
-export const $selectedPageHash = atom<string>("");
-
-export const $editingPageId = atom<undefined | Page["id"]>();
--- packages/sdk/src/runtime.ts
+++
@@ -1,3 +0,0 @@
-export * from "./resource-loader";
-export * from "./to-string";
-export * from "./form-fields";
